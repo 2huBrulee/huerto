fc74b78d3e2c3ea9723a5e8a015824ec0b0d8544
Cesar Gutierrez <cesar@tineo.mobi>
2018-06-23 03:59:47 -0500

    add serial

--- listener.rb
+++ listener.rb
@@ -1,20 +1,38 @@
 require 'rubyserial'
+require 'serialport'
 require './sender_reading'
 require './sender_average'
 
 class Listener
     def initialize
-        @arduino = Serial.new '/dev/ttyACM0'
+        #@arduino = Serial.new '/dev/ttyACM0'
+
+        @port_str = "/dev/ttyACM0"
+        @baud_rate = 9600
+        @data_bits = 8
+        @stop_bits = 1
+        @parity = SerialPort::NONE
+        @sp = SerialPort.new(@port_str, @baud_rate, @data_bits, @stop_bits, @parity)
+
+        @sp.flush()
     end
 
     def readSerial
-        @lectura = @arduino.read(256)
-        if @lectura.nil?
-            puts 'No ha mandado nada el arduino'
-        else
-            puts @lectura
+        #@lectura = @arduino.read(256)
+        #if @lectura.nil?
+        #    puts 'No ha mandado nada el arduino'
+        #else
+        #    puts @lectura
             #parse data
+        #end
+
+        @sp.flush()
+        if (i = @sp.gets.chomp)
+            puts i
+            lectura = i
         end
+
+        lectura
     end
 
     def readCamera(unix_time)
@@ -28,7 +46,8 @@ class Listener
         loop do
             unix_time = Time.now.to_i
             fn = self.readCamera(unix_time)
-            self.readSerial
+            l = self.readSerial
+            puts "l:#{l}"
 
             s = SenderReading.new(:unix_time => unix_time, :file_name => fn)
             link = s.sendDrive()
